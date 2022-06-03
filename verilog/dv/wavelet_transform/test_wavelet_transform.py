aea48b3ad0cb597e19316fdb2324208a163c349a
opensource <opensource@foodrev.org>
2022-06-04 01:47:31 +0200

    Add specific tests for audio and mod_sine

--- verilog/dv/wavelet_transform/test_wavelet_transform.py
+++ verilog/dv/wavelet_transform/test_wavelet_transform.py
@@ -31,55 +31,47 @@ async def test_start(dut):
     await ClockCycles(dut.clk, 80)
     dut.RSTB.value = 1
 
-    # wait for the project to become active
-    # in the testbench use a utility wire
+    # Before starting first module test wait for sequence of falling then
+    # running edge of 'o_active' signal.
+    await with_timeout(FallingEdge(dut.o_active), 700, 'us')
     await with_timeout(RisingEdge(dut.o_active), 700, 'us')
 
-
 @cocotb.test()
-async def test_wavelet(dut):
+async def test_audio(dut):
     clock = Clock(dut.clk, 25, units="ns")
     cocotb.fork(clock.start())
 
-
-    # TODO: Assert proper starting values for wavelet_transform
-    # # e.g. these should all be low at start
-    # assert dut.pwm0_out.value == 0
-    # assert dut.pwm1_out.value == 0
-    # assert dut.pwm1_out.value == 0
-
-    # open audio files for read and write
-    # audio_in = wave.open("./test/middle_c.wav")
-    # QUESTION: what path should these wavs be in? (relative to which directory? makefile dir?)
-    # relative to makefile
-
-    # undo this comment out to use audio
-    # audio_in = wave.open("./wavs/hello.wav")
-    # audio_out = wave.open("./wavs/out.wav", "wb")
-    # audio_out.setnchannels(audio_in.getnchannels())
-    # audio_out.setsampwidth(audio_in.getnchannels())
-    # audio_out.setframerate(audio_in.getframerate())
-
-    # nframes = audio_in.getnframes()
-    # print("sending %d frames" % nframes)
-
+    # These should be held low by default
     dut.i_data_clk.value = 0
-    counter = 0
-    dut.i_select_output_channel.value = 0 % 8
+    dut.i_value.value = 0
+    dut.i_select_output_channel.value = 0
+
+    await RisingEdge(dut.clk)
+
+    # output should be zero if prior values are zero
+    assert dut.o_multiplexed_wavelet_out == 0
+
+    # audio input setup
+    audio_in = wave.open("./wavs/hello.wav")
+    nframes = audio_in.getnframes()
+    print("sending %d frames" % nframes)
+
+    # audio output setup
+    audio_out = wave.open("./wavs/out.wav", "wb")
+    audio_out.setnchannels(audio_in.getnchannels())
+    audio_out.setsampwidth(audio_in.getnchannels())
+    audio_out.setframerate(audio_in.getframerate())
+
+    # values for switching the output channel
     channel_select = 0
     channel_select_counter = 200
 
     # process audio through dut
-    # for i in range(nframes):
-    for i in range(5000):
+    for i in range(nframes):
         await RisingEdge(dut.clk)
 
-        # frame = audio_in.readframes(1)
-        # print(frame)
-
-        # (val,) = struct.unpack("h", frame)
-
-        dut.i_value.value = int(math.sin(0.1*counter*(1 + 0.01*counter))  * 127)
+        frame = audio_in.readframes(1)
+        (val,) = struct.unpack("h", frame)
 
         if channel_select_counter == 0:
             channel_select += 1
@@ -88,8 +80,6 @@ async def test_wavelet(dut):
         else:
             channel_select_counter -= 1
 
-        dut.i_value.value = int(math.sin(0.1*counter*(1 + 0.01*counter))  * 127)
-        counter = 1 + counter
         # print(dut.i_value.value)
         # if val > 0:
         #     dut.i_value.value = min(int((val / 32768.0 * 127)), 127)
@@ -130,3 +120,115 @@ async def test_wavelet(dut):
         # assert(True)
         # raw_out, = struct.pack('i', dut.o_sum[0].value.signed_integer)
         # audio_out.writeframes(raw_out)
+
+@cocotb.test()
+async def test_modulated_sine(dut):
+
+    clock = Clock(dut.clk, 25, units="ns")
+    cocotb.fork(clock.start())
+
+    # These should be held low by default
+    dut.i_data_clk.value = 0
+    dut.i_value.value = 0
+    dut.i_select_output_channel.value = 0
+
+    await RisingEdge(dut.clk)
+
+    # output should be zero if prior values are zero
+    assert dut.o_multiplexed_wavelet_out == 0
+
+    counter = 0
+    channel_select = 0
+    channel_select_counter = 200
+
+    # process audio through dut
+    for i in range(5000):
+        await RisingEdge(dut.clk)
+
+        if channel_select_counter == 0:
+            channel_select += 1
+            dut.i_select_output_channel.value = channel_select % 8
+            channel_select_counter = 200
+        else:
+            channel_select_counter -= 1
+
+        dut.i_value.value = max(min(int(math.sin(0.1*counter*(1 + 0.01*counter))  * 128), 127),-128)
+        counter = 1 + counter
+
+        # clk in data
+        dut.i_data_clk.value = 1
+
+        await RisingEdge(dut.clk)
+
+        dut.i_data_clk.value = 0
+
+        await RisingEdge(dut.clk)
+        await RisingEdge(dut.clk)
+        await RisingEdge(dut.clk)
+        await RisingEdge(dut.clk)
+
+@cocotb.test()
+async def test_modulated_sine_overflow(dut):
+
+    clock = Clock(dut.clk, 25, units="ns")
+    cocotb.fork(clock.start())
+
+    # These should be held low by default
+    dut.i_data_clk.value = 0
+    dut.i_value.value = 0
+    dut.i_select_output_channel.value = 0
+
+    await RisingEdge(dut.clk)
+
+    # output should be zero if prior values are zero
+    assert dut.o_multiplexed_wavelet_out == 0
+
+    counter = 0
+    channel_select = 0
+    channel_select_counter = 200
+
+    # process audio through dut
+    for i in range(5000):
+        await RisingEdge(dut.clk)
+
+        # create sine wave value within [127, -128] (signed 8-bit int)
+        dut.i_value.value = max(min(int(math.sin(0.1*counter*(1 + 0.001*counter))  * 128), 127),-128)
+
+        if channel_select_counter == 0:
+            channel_select += 1
+            dut.i_select_output_channel.value = channel_select % 8
+            channel_select_counter = 200
+        else:
+            channel_select_counter -= 1
+
+        counter = 1 + counter
+
+        # clk in data
+        dut.i_data_clk.value = 1
+
+        # print(val, dut.i_value.value)
+        # print(dut.i_value)
+        await RisingEdge(dut.clk)
+
+        dut.i_data_clk.value = 0
+
+        # print(int(str(dut.o_sum),2))
+        # s = int(str(dut.o_sum[0:31]), 2)
+
+        # if s > 2147483647:
+        #     s = s - 4294967295 - 1
+
+        # input = 0
+        # if int(dut.i_value) > 127:
+        #     input = int(str(dut.i_value), 2) - 255 - 1
+        # else:
+        #     input = int(str(dut.i_value), 2)
+
+
+#         print(val, input, s)
+        # print(val, dut.i_value, s)
+
+        await RisingEdge(dut.clk)
+        await RisingEdge(dut.clk)
+        await RisingEdge(dut.clk)
+        await RisingEdge(dut.clk)
